4b68b04921129308e61d4136c9c61bd922f56cc2
Rauan Santos Moreira <rmoreira@segware.com>
2026-05-15 20:07:00 -0300

    firt commit

---
+++ src/test/java/com/aws/assurance/class3/Class3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.assurance.class3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Class3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
